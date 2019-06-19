6f4fdd74e56c56bf2da45df6250a73e661ffdbaa
MichalAW <michal.wantaeuco@gmail.com>
2019-06-19 18:59:34 +0200

    plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,54 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-       rules: [{
-               test: /\.js$/,
-               loader: "babel-loader"
-           },
-           {
-               test: /\.css$/,
-               use: [{
-                       loader: 'style-loader'
-                   },
-                   {
-                       loader: 'css-loader',
-                       options: {
-                           modules: true
-                       }
-                   }
-               ]
-           }
-       ]
-    },
-    devServer: {
-        contentBase: './build',
-        publicPath: '/',
-        historyApiFallback: {
-            index: '/index.html'
+module.exports = function (env) {
+    return {
+        mode: env.production ? 'production' : 'development',
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.bundle.js'
         },
-        port: 3000,
-        hot: true,
-        compress: false,
-        disableHostCheck: true,
+        optimization: {
+            minimizer: env.production ? [
+                new TerserPlugin({
+                    cache: true,
+                    parallel: true,
+                    sourceMap: true, 
+                    terserOptions: {
+                    }
+                }),
+            ] : [],
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+        },
+        devServer: {
+            contentBase: './build',
+            publicPath: '/',
+            historyApiFallback: {
+                index: '/index.html'
+            },
+            port: 3000,
+            hot: true,
+            compress: false,
+            disableHostCheck: true,
+        }
     }
 };
